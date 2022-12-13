91cff6c371a3ee8227b1a8dd3a2f8aef1064f5db
ilyas hammou <ilyashammou.prof@gmail.com>
2022-12-13 08:48:57 +0100

    first commit

---
+++ src/main/java/com/example/ateliermaven/AtelierMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.ateliermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtelierMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ateliermaven/AtelierMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ateliermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
